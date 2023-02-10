bcdbe88025f5125a0145f6120428db12caffe158
jmcshane <james.mcshane@superorbital.io>
2023-02-10 15:27:43 -0500

    Pull digest and set on image

--- .buildkite/steps/image.sh
+++ .buildkite/steps/image.sh
@@ -3,3 +3,7 @@ set -euxo pipefail
 
 git config --global --add safe.directory "$(pwd)"
 /cnb/lifecycle/creator -app "$(pwd)" -report /tmp/report.toml ghcr.io/superorbital/cnb-sample-go:latest
+
+DIGEST=$(cat /tmp/report.toml  | grep digest | awk '{print $3}' | tr -d '"')
+
+buildkite-agent meta-data set "image" "ghcr.io/superorbital/cnb-sample-go@${DIGEST}"
